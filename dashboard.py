5b1a6dfcc1050247b02be819b5639d4b39c9a724
Faraj11 <44307319+Faraj11@users.noreply.github.com>
2025-09-11 11:33:32 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -240,12 +240,10 @@ with col[2]:
 
     with st.expander('About', expanded=True):
     st.write(
-        f"""
-        - Data: [U.S. Census Bureau](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html).
+        f"""- Data: [U.S. Census Bureau](https://www.census.gov/data/datasets/time-series/demo/popest/2010s-state-total.html).
         - :orange[**Gains/Losses**]: states with high inbound/outbound migration for the selected year
-        - :orange[**States Migration**]: percentage of states with annual inbound/outbound migration > {format_number(MIGRATION_THRESHOLD)}
-        """
-    )
+        - :orange[**States Migration**]: percentage of states with annual inbound/outbound migration > {format_number(MIGRATION_THRESHOLD)}""")
+
 
 
 
